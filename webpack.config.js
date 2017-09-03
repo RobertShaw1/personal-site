8ac426fb2689c27967e49add131d358f8c0633b3
Robert Shaw <r.shaw1@yahoo.com>
2017-09-03 11:54:13 -0500

    added node modules, webpack config, babelrc, and gitignore

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './app/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.(jsx)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]},
+      { test: /\.(png|jpg|gif)$/, use: 'file-loader' },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    })
+  ]
+};
